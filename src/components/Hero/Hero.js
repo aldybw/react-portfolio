1425c2b8e474b7355506acd4eaaa24213e1ba6c1
aldyckybw <bagusaldycky@gmail.com>
2023-01-23 21:16:48 +0700

    hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,6 +8,23 @@ import {
 import Button from "../../styles/GlobalComponents/Button";
 import { LeftSection } from "./HeroStyles";
 
-const Hero = (props) => <div>Hero</div>;
+const Hero = (props) => (
+  <Section row nonpadding>
+    <LeftSection>
+      <SectionTitle>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        My name is Aldycky Bagus Witjaksana. I'm mostly doing Backend
+        Development to provide REST API and sometimes I'm also doing Frontend
+        Development especially for Website Application.
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
+);
 
 export default Hero;
